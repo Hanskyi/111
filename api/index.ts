fd264148bf07c48ef5addcd882a2c72d2cb175fe
hanskyi <han.eginbaev@gmail.com>
2024-01-07 22:21:21 +0600

    vercel

--- api/index.ts
+++ api/index.ts
@@ -13,6 +13,8 @@ const port = 8000;
 app.use(cors(
     {
         origin: ['http://localhost:3000'],
+        methods:["POST", "GET", "DELETE"],
+        credentials: true
     }
 ));
 app.use(express.static('public'));
