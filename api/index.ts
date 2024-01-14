d7aa984232bab28efa12d0904b240e6e461ece89
hanskyi <han.eginbaev@gmail.com>
2024-01-14 13:03:09 +0600

    vercel

--- api/index.ts
+++ api/index.ts
@@ -17,27 +17,6 @@ app.use(cors(
         credentials: true
     }
 ));
-
-app.use((req, res, next) => {
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "http://localhost:3000"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Private-Network", "true");
-
-    res.setHeader("Access-Control-Max-Age", "7200");
-
-    next();
-});
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/posts', postRouter);
